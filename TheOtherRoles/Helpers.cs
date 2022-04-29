6faa8af4463a5338caa9061beac9947d9f90d5ae
tanakanira0118 <github@tanakanira.com>
2022-04-29 20:25:17 +0900

    showFlashメソッドを追加

--- TheOtherRoles/Helpers.cs
+++ TheOtherRoles/Helpers.cs
@@ -603,6 +603,28 @@ namespace TheOtherRoles {
             return team;
         }
 
+        public static void showFlash(Color color, float duration = 1f)
+        {
+            if (HudManager.Instance == null || HudManager.Instance.FullScreen == null) return;
+            HudManager.Instance.FullScreen.gameObject.SetActive(true);
+            HudManager.Instance.FullScreen.enabled = true;
+            HudManager.Instance.StartCoroutine(Effects.Lerp(duration, new Action<float>((p) => {
+                var renderer = HudManager.Instance.FullScreen;
+
+                if (p < 0.5)
+                {
+                    if (renderer != null)
+                        renderer.color = new Color(color.r, color.g, color.b, Mathf.Clamp01(p * 2 * 0.75f));
+                }
+                else
+                {
+                    if (renderer != null)
+                        renderer.color = new Color(color.r, color.g, color.b, Mathf.Clamp01((1 - p) * 2 * 0.75f));
+                }
+                if (p == 1f && renderer != null) renderer.enabled = false;
+            })));
+        }
+
         public static void shuffle<T>(this IList<T> self, int startAt = 0)
         {
             for (int i = startAt; i < self.Count - 1; i++) {
