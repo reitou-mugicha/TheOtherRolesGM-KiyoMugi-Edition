ea028f8a596e36000e7b0aea5cd5227382e02350
Haoming <nh.haoming@gmail.com>
2022-05-05 19:24:23 +0900

    GitlabCI対応

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -23,7 +23,7 @@ namespace TheOtherRoles
     {
         public const string Id = "me.eisbison.theotherroles";
 
-        public const string VersionString = "2.0.13";
+        public const string VersionString = "999.999.999";
         
         public static System.Version Version = System.Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
