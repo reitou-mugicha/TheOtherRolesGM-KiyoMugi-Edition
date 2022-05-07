e1eb796a84782f7d8558fd6021c249690e41792e
tanakanira0118 <github@tanakanira.com>
2022-05-07 17:00:02 +0900

    receivesに修正

--- TheOtherRoles/Patches/PlayerControlPatch.cs
+++ TheOtherRoles/Patches/PlayerControlPatch.cs
@@ -1250,7 +1250,7 @@ namespace TheOtherRoles.Patches
                 RPCProcedure.sidekickPromotes();
             }
 
-            // Pursuer promotion trigger on murder (the host sends the call such that everyone recieves the update before a possible game End)
+            // Pursuer promotion trigger on murder (the host sends the call such that everyone receives the update before a possible game End)
             if (target == Lawyer.target && AmongUsClient.Instance.AmHost)
             {
                 MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.LawyerPromotesToPursuer, Hazel.SendOption.Reliable, -1);
@@ -1422,7 +1422,7 @@ namespace TheOtherRoles.Patches
                 RPCProcedure.sidekickPromotes();
             }
 
-            // Pursuer promotion trigger on exile (the host sends the call such that everyone recieves the update before a possible game End)
+            // Pursuer promotion trigger on exile (the host sends the call such that everyone receives the update before a possible game End)
             if (__instance == Lawyer.target && AmongUsClient.Instance.AmHost)
             {
                 MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.LawyerPromotesToPursuer, Hazel.SendOption.Reliable, -1);
