1c2ae82b4768e6f9187777a96c3c159cb6f99b6e
tanakanira0118 <github@tanakanira.com>
2022-05-09 01:43:44 +0900

    ラウンジ配線のオブジェクト名を修正

--- TheOtherRoles/Patches/AirshipPatch.cs
+++ TheOtherRoles/Patches/AirshipPatch.cs
@@ -65,7 +65,7 @@ namespace TheOtherRoles.Patches
                 ActivateWiring("task_wiresHallway2", 2);
                 ActivateWiring("task_electricalside2", 3).Room = SystemTypes.Armory;
                 ActivateWiring("task_wireShower", 4);
-                ActivateWiring("task_wiresLounge", 5);
+                ActivateWiring("taks_wiresLounge", 5);
                 ActivateWiring("panel_wireHallwayL", 6);
                 ActivateWiring("task_wiresStorage", 7);
                 ActivateWiring("task_electricalSide", 8).Room = SystemTypes.VaultRoom;
