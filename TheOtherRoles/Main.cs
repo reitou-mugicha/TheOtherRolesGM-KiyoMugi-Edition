e6e2f838f835ec5f7f98e515518735b3b22a0db8
Dekokiyo <97837904+Dekokiyo@users.noreply.github.com>
2022-05-15 09:45:28 +0900

    Fix Chat Bugs

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -197,5 +197,29 @@ namespace TheOtherRoles
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
+
+        [HarmonyPatch(typeof(ChatController), nameof(ChatController.Update))]
+        public static class ChatControllerAwakePatch
+        {
+            public static void Prefix()
+            {
+                SaveManager.chatModeType = 1;
+                SaveManager.isGuest = false;
+            }
+            public static void Postfix(ChatController __instance)
+            {
+                SaveManager.chatModeType = 1;
+                SaveManager.isGuest = false;
+                if (Input.GetKeyDown(KeyCode.F1))
+                {
+                    if (!__instance.isActiveAndEnabled) return;
+                    __instance.SetVisible(false);
+                    new LateTask(() =>
+                    {
+                        __instance.SetVisible(true);
+                    }, 0f, "AntiChatBag");
+                }
+            }
+        }
     }
 }
