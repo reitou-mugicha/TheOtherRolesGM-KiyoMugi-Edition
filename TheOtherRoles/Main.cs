70e616c7ea6538ea560ca788e5ec651c573f6fbe
Dekokiyo <dekobokokiyomori0817@gmail.com>
2022-06-14 16:56:03 +0900

    2.2.0

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -22,7 +22,7 @@ namespace TheOtherRoles
     public class TheOtherRolesPlugin : BasePlugin
     {
         public const string Id = "kiyomori.mugicha.theotherrolesgmkm";
-        public const string VersionString = "2.1.2";
+        public const string VersionString = "2.2.0";
         public static System.Version Version = System.Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
 
