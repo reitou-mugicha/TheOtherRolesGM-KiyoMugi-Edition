9b1bb5a8eae58863ef7e04382b513cfb7497a964
Haoming <nh.haoming@gmail.com>
2022-07-06 19:52:32 +0900

    イビルトラッカー会議中ボタンの色を変更

--- TheOtherRoles/Patches/MeetingPatch.cs
+++ TheOtherRoles/Patches/MeetingPatch.cs
@@ -666,6 +666,7 @@ namespace TheOtherRoles.Patches
                     targetBox.transform.localPosition = new Vector3(-0.95f, 0.03f, -1.3f);
                     SpriteRenderer renderer = targetBox.GetComponent<SpriteRenderer>();
                     renderer.sprite = EvilTracker.getArrowSprite();
+                    renderer.color = Palette.CrewmateBlue;
                     PassiveButton button = targetBox.GetComponent<PassiveButton>();
                     button.OnClick.RemoveAllListeners();
                     int copiedIndex = i;
