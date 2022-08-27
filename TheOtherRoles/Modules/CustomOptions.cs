ea83a8e0e509898b500fdca058511bc80dfb8d3a
reitou-mugicha <kounoaoto1029@outlook.jp>
2022-08-28 01:22:08 +0900

    マップ選べる設定を削除

--- TheOtherRoles/Modules/CustomOptions.cs
+++ TheOtherRoles/Modules/CustomOptions.cs
@@ -819,44 +819,7 @@ namespace TheOtherRoles
         }
     }
 
-    [HarmonyPatch(typeof(GameSettingMenu), nameof(GameSettingMenu.Start))]
-    class GameSettingMenuStartPatch
-    {
-        public static void Prefix(GameSettingMenu __instance)
-        {
-            __instance.HideForOnline = new Transform[] { };
-        }
 
-        public static void Postfix(GameSettingMenu __instance)
-        {
-            // Setup mapNameTransform
-            var mapNameTransform = __instance.AllItems.FirstOrDefault(x => x.name.Equals("MapName", StringComparison.OrdinalIgnoreCase));
-            if (mapNameTransform == null) return;
-
-            var options = new Il2CppSystem.Collections.Generic.List<Il2CppSystem.Collections.Generic.KeyValuePair<string, int>>();
-            for (int i = 0; i < Constants.MapNames.Length; i++)
-            {
-                // Dleks was removed from the game, so remove it from our selections.
-                if (i == (int)MapNames.Dleks) continue;
-
-                var kvp = new Il2CppSystem.Collections.Generic.KeyValuePair<string, int>();
-                kvp.key = Constants.MapNames[i];
-                kvp.value = i;
-                options.Add(kvp);
-            }
-            mapNameTransform.GetComponent<KeyValueOption>().Values = options;
-            mapNameTransform.gameObject.active = true;
-
-            foreach (Transform i in __instance.AllItems.ToList())
-            {
-                float num = -0.5f;
-                if (i.name.Equals("MapName", StringComparison.OrdinalIgnoreCase)) num = -0.25f;
-                if (i.name.Equals("NumImpostors", StringComparison.OrdinalIgnoreCase) || i.name.Equals("ResetToDefault", StringComparison.OrdinalIgnoreCase)) num = 0f;
-                i.position += new Vector3(0, num, 0);
-            }
-            __instance.Scroller.ContentYBounds.max += 0.5F;
-        }
-    }
 
     [HarmonyPatch(typeof(Constants), nameof(Constants.ShouldFlipSkeld))]
     class ConstantsShouldFlipSkeldPatch
